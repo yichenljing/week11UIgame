4628225a71ec3f6d5f05822b08069057553dcd79
yichenljing <40904846+yichenljing@users.noreply.github.com>
2019-11-26 23:34:15 -0500

    add button

---
+++ week11UIgame/Assets/script/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject NameInputField;
+    public GameObject NameButton;
+
+    public void TurnOnNameButton() {
+        NameButton.SetActive(true);
+
+
+
+    }
+}
